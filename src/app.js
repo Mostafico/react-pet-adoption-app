bce93c5fea2995e73a6e8d19e23dd94f4375f8fd
Mostafico <mostafico@gmail.com>
2022-02-20 17:16:51 +0200

    Added Code Splitting

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 import ReactDOM from "react-dom";
-import { StrictMode, useState } from "react";
+import { StrictMode, useState, lazy, Suspense } from "react";
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
-import SearchParams from "./SearchParams";
-import Details from "./Details";
 import ThemeContext from "./ThemeContext";
 
 // const App = () => {
@@ -26,26 +24,31 @@ import ThemeContext from "./ThemeContext";
 //   ]);
 // };
 
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
+
 const App = () => {
   const theme = useState("darkblue");
   return (
     <ThemeContext.Provider value={theme}>
       <div>
-        <Router>
-          <header>
-            <Link to="/">
-              <h1 id="my-brand">Adopt Me!</h1>
-            </Link>
-          </header>
-          <Switch>
-            <Route path="/details/:id">
-              <Details />
-            </Route>
-            <Route path="/">
-              <SearchParams />
-            </Route>
-          </Switch>
-        </Router>
+        <Suspense fallback={<h2>Loading Route</h2>}>
+          <Router>
+            <header>
+              <Link to="/">
+                <h1 id="my-brand">Adopt Me!</h1>
+              </Link>
+            </header>
+            <Switch>
+              <Route path="/details/:id">
+                <Details />
+              </Route>
+              <Route path="/">
+                <SearchParams />
+              </Route>
+            </Switch>
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   );
